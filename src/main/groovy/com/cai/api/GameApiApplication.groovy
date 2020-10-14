e34f1af7fb9455e31a910735f5d5b5a3e2a4b0bb
cai <942445346@qq.com>
2020-10-14 15:39:36 +0800

    web更换版本 修改IgnoreAuth的bug

--- src/main/groovy/com/cai/api/GameApiApplication.groovy
+++ src/main/groovy/com/cai/api/GameApiApplication.groovy
@@ -1,6 +1,5 @@
 package com.cai.api
 
-import com.cai.web.core.AsyncPrintLogConsumer
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
