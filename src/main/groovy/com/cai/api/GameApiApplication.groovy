70cfe0147f6c4d0748e97aff86d226bd553b702d
cai <942445346@qq.com>
2021-01-15 18:48:23 +0800

    排除bug。。。。

--- src/main/groovy/com/cai/api/GameApiApplication.groovy
+++ src/main/groovy/com/cai/api/GameApiApplication.groovy
@@ -2,12 +2,13 @@ package com.cai.api
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.ComponentScan
 
 @ComponentScan("com.cai")
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration)
 class GameApiApplication extends SpringBootServletInitializer {
 
     @Override
